67cb1ef08389846323b7ed1052ee6b4f66399b2e
Enrico Serrano <69224805+enricoserrano@users.noreply.github.com>
2022-08-10 20:55:27 +1200

    commit message

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const books = require('./routes/api/books');
 const app = express();
-const mongoose = require("mongoose");
-require("dotenv").config();
-connectDB();
 
-mongoose
-  .connect(
-    process.env.MONGODB_CONNECTION_STRING,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("MongoDB has been connected"))
-  .catch((err) => console.log(err));
+connectDB();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
@@ -25,9 +13,7 @@ app.use(express.static("build"));
 
 const path = require("path");
 
-// Step 1:
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-// Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 });
