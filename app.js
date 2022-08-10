8b78f2c296e9863b3c042b1f847b9a3b5e4dccca
Enrico Serrano <69224805+enricoserrano@users.noreply.github.com>
2022-08-10 20:44:07 +1200

    heroku 2nd commit

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 });
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
